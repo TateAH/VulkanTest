7bd52b129dd87e42c9573307a1a6d6ba19de46de
TateH <32803993+TateAH@users.noreply.github.com>
2019-10-09 11:20:39 -0400

    Add files via upload

---
+++ CompileShaders.bat
@@ -0,0 +1,3 @@
+C:\VulkanSDK\1.1.121.0\Bin\glslc.exe VulkanTest\shaders\shader.vert -o VulkanTest\shaders\vert.spv
+C:\VulkanSDK\1.1.121.0\Bin\glslc.exe VulkanTest\shaders\shader.frag -o VulkanTest\shaders\frag.spv
+pause
